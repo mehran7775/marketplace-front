5d6b41a5994b34f095afc7816e30a7c1647690a3
mehran yousefi <u3efimehran@gmail.com>
2021-10-25 14:21:57 +0330

    using anothe toster and resolve more styling on pages v3

--- pages/_store_slug/index.vue
+++ pages/_store_slug/index.vue
@@ -25,84 +25,28 @@ export default {
       return tr();
     },
   },
-  data(){
-    return{
-      products:[
-
-        {
-          "id": "zpg3ergr",
-          "title": "محصول",
-          "thumbnail": "https://coreshop.paystar.ir/storage/images/Se70rRop9RiMLI8Jld3yzoA380IoOSCx7q3Gm4wY.png",
-          "description": null,
-          "price": "10,000",
-          "quantity": 20
-        },
-        {
-          "id": "vr9retkj",
-          "title": "vbvvb",
-          "thumbnail": "https://coreshop.paystar.ir/storage/images/zXGPi0Z02UwyELxHxHhcYWpeGsuDZaKIkE2xZf5n.png",
-          "description": null,
-          "price": "20,000",
-          "quantity": 20
-        },
-        {
-          "id": "vgr9rgj",
-          "title": "vbvvb",
-          "thumbnail": "https://coreshop.paystar.ir/storage/images/zXGPi0Z02UwyELxHxHhcYWpeGsuDZaKIkE2xZf5n.png",
-          "description": null,
-          "price": "20,000",
-          "quantity": 20
-        },
-        {
-          "id": "vr9kgthajj",
-          "title": "vbvvb",
-          "thumbnail": "https://coreshop.paystar.ir/storage/images/zXGPi0Z02UwyELxHxHhcYWpeGsuDZaKIkE2xZf5n.png",
-          "description": null,
-          "price": "20,000",
-          "quantity": 20
-        },
-        {
-          "id": "vr39rgj",
-          "title": "vbvvb",
-          "thumbnail": "https://coreshop.paystar.ir/storage/images/zXGPi0Z02UwyELxHxHhcYWpeGsuDZaKIkE2xZf5n.png",
-          "description": null,
-          "price": "20,000",
-          "quantity": 20
-        },
-        {
-          "id": "vr90kthjj",
-          "title": "vbvvb",
-          "thumbnail": "https://coreshop.paystar.ir/storage/images/zXGPi0Z02UwyELxHxHhcYWpeGsuDZaKIkE2xZf5n.png",
-          "description": null,
-          "price": "20,000",
-          "quantity": 20
-        },
-      ]
+  async asyncData({ $axios, route, error }) {
+    try {
+      const { data } = await $axios.get(
+        `/store/${route.params.store_slug}/products`
+      );
+      return {
+        products: data.data.data
+      };
+    } catch (e) {
+      if (e.response) {
+        error({
+          statusCode: e.response.status,
+          message: e.response.message,
+        });
+      } else {
+        error({
+          statusCode: "",
+          message: "خطا در ارتباط",
+        });
+      }
     }
-
-  }
-  // async asyncData({ $axios, route, error }) {
-  //   try {
-  //     const { data } = await $axios.get(
-  //       `/store/${route.params.store_slug}/products`
-  //     );
-  //     return {
-  //       products: data.data.data
-  //     };
-  //   } catch (e) {
-  //     if (e.response) {
-  //       error({
-  //         statusCode: e.response.status,
-  //         message: e.response.message,
-  //       });
-  //     } else {
-  //       error({
-  //         statusCode: "",
-  //         message: "خطا در ارتباط",
-  //       });
-  //     }
-  //   }
-  // },
+  },
 };
 </script>
 
