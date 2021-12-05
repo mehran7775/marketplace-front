dcf4c78aeab8e0128a30ff2e3326cb573b6cd04b
mehran yousefi <u3efimehran@gmail.com>
2021-12-05 14:56:48 +0330

    alter withs storySection

--- components/molecules/Xheader.vue
+++ components/molecules/Xheader.vue
@@ -71,15 +71,21 @@
                 pagination-padding="2"
               >
                 <slide class="img-wrapper p-4">
-                 <div>
+                 <div class="d-flex align-items-center">
+                   <img class="rounded-circle" width="48" height="48" :src="detail.logo" :alt="`لوگوی فروشگاه ${detail.fa_name}`">
                    <h2 class="h5 font-weight-bold">توضیحات فروشگاه:</h2>
+                 </div>
+                 <div class="py-4 px-4">
                    <p v-text="detail.description"></p>
                  </div>
                 </slide>
                 <slide class="img-wrapper p-4">
-                 <div>
+                <div class="d-flex align-items-center">
+                   <img class="rounded-circle" width="48" height="48" :src="detail.logo" :alt="`لوگوی فروشگاه ${detail.fa_name}`">
                    <h2 class="h5 font-weight-bold">قوانین فروشگاه:</h2>
-                   <p></p>
+                 </div>
+                 <div class="py-4 px-4">
+                   <p v-text="detail.description"></p>
                  </div>
                 </slide>
               </carousel>
@@ -136,6 +142,7 @@ header {
     transition: color 0.2s;
     cursor: pointer;
   }
+
   @include medium {
     box-shadow: 0 0 6px 0 $silver;
     border-bottom: 1px solid $silver;
@@ -191,14 +198,10 @@ header {
   background: $back_dark;
   z-index: 9999;
   .story{
-   @include medium{
-    max-width: 550px;
-    min-width: 500px;
-   }
-    max-width: 350px;
-    min-width: 330px;
+    width: 70vw;
     height: 80vh;
-    background-color: whitesmoke;
+    background-color: aqua;
+    background-color: $white;
 
     .img-wrapper {
       height: 80vh;
