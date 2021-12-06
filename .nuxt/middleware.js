71d36339f2d6eb0f4838d405a7ff02008b74f897
ali hanafi <alihanafi4work@gmail.com>
2021-12-06 09:18:22 +0000

    delete .nuxt

--- .nuxt/middleware.js
+++
@@ -1,9 +0,0 @@
-const middleware = {}
-
-middleware['checkAuth'] = require('..\\middleware\\checkAuth.js')
-middleware['checkAuth'] = middleware['checkAuth'].default || middleware['checkAuth']
-
-middleware['guest'] = require('..\\middleware\\guest.js')
-middleware['guest'] = middleware['guest'].default || middleware['guest']
-
-export default middleware
